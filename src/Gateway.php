2bdfe1d38a05b4e006c2837d0a50e0924fb88ab4
Tarun M <tmukherjee13@gmail.com>
2020-05-01 22:08:19 +0530

    Initial commit

--- src/Gateway.php
+++ src/Gateway.php
@@ -3,6 +3,8 @@
 namespace Omnipay\Saferpay;
 
 use Omnipay\Common\AbstractGateway;
+use Omnipay\Common\Helper;
+use Symfony\Component\HttpFoundation\ParameterBag;
 
 class Gateway extends AbstractGateway
 {
@@ -13,8 +15,64 @@ class Gateway extends AbstractGateway
 
     public function getDefaultParameters()
     {
-        return [];
+        $settings = parent::getDefaultParameters();
+        return $settings;
     }
+
+    public function initialize(array $parameters = array())
+    {
+        $this->parameters = new ParameterBag;
+
+        // set default parameters
+        foreach ($this->getDefaultParameters() as $key => $value) {
+            if (is_array($value)) {
+                $this->parameters->set($key, reset($value));
+            } else {
+                $this->parameters->set($key, $value);
+            }
+        }
+
+        Helper::initialize($this, $parameters);
+        return $this;
+    }
+
+    public function getAccountId()
+    {
+        return $this->getParameter('accountId');
+    }
+
+    public function setAccountId($value)
+    {
+        return $this->setParameter('accountId', $value);
+    }
+
+    public function setUsername($username)
+    {
+        $this->setParameter('username', $username);
+    }
+
+    public function getUsername()
+    {
+        return $this->getParameter('username');
+    }
+
+    public function setPassword($password)
+    {
+        $this->setParameter('password', $password);
+    }
+
+    public function getPassword()
+    {
+        return $this->getParameter('password');
+    }
+
+    public function getAuthCredentials()
+    {
+        $this->validate('username', 'password');
+
+        return base64_encode($this->getUsername() . ':' . $this->getPassword());
+    }
+
 
     /**
      * Create an authorize request.
